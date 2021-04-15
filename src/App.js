f1928abe7cce2231d04e3ca576b3f1618e786dc2
subhash-star <subhamjoshibtech@gmail.com>
2021-04-15 17:51:39 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import axios from "axios";
 import ProductForm from "./Component/ProductForm";
 import "./App.css";
 // const apiURL = "http://localhost:4000";
-// const apiURL = "https://buy-n-sell--app.herokuapp.com";
-const apiURL = "";
+const apiURL = "https://buy-n-sell--app.herokuapp.com";
+// const apiURL = "";
 
 const App = () => {
   const [isLoggedIn, setisLoggedIn] = useState(false);
